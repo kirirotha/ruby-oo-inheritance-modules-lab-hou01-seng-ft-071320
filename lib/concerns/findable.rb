aa6f918208b312d0134e08515b4dbd907ec9b80f
Kiri Um <kirirotha@gmail.com>
2020-07-21 20:07:22 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,9 @@
 
 module Findable
 
-  def find_by_name
-    def self.find_by_name(name)
-      @@songs.detect{|a| a.name == name}
-    end
+  def find_by_name(name)
+      Song.all.detect{|a| a.name == name}
+      Artist.all.detect{|a| a.name == name}
+
   end
 end
