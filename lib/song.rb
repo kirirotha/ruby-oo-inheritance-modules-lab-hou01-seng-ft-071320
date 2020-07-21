03fd8cbf71f10fba195331ad86aa7594edb1d103
Kiri Um <kirirotha@gmail.com>
2020-07-21 20:25:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,8 @@ require 'pry'
 #require_relative '../concerns/memorable.rb'
 
 class Song
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
   attr_accessor :name
